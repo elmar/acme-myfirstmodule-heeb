31261026302d441520a31dbd79928373ec3f5adb
Elmar S. Heeb <elmar@heebs.ch>
2014-09-06 11:30:56 +0200

    make new version with fix

--- lib/Acme/MyFirstModule/HEEB.pm
+++ lib/Acme/MyFirstModule/HEEB.pm
@@ -10,11 +10,11 @@ Acme::MyFirstModule::HEEB - The great new Acme::MyFirstModule::HEEB!
 
 =head1 VERSION
 
-Version 0.01.01
+Version 0.01.02
 
 =cut
 
-our $VERSION = '0.01.01';
+our $VERSION = '0.01.02';
 
 
 =head1 SYNOPSIS
